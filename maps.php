995f5160d6d28489925d122776638dba6a58c49b
Ranjitha <32982928+RanjithAAAA@users.noreply.github.com>
2019-12-10 11:44:23 +0530

    Update maps.php

--- maps.php
+++ maps.php
@@ -23,7 +23,21 @@ function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude;
 }
-</script>
 
-</body>
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -2.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 8, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    </script>
+   
+      <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBpXro1ICwJd6g7HD79mW_Kq1wZeiwp3j0&callback=initMap"
+  type="text/javascript"></script>
+  </body>
 </html>
