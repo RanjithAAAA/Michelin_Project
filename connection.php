eb3054ed6289211d6e57f562e2fc9660d441bc94
Ranjitha <32982928+RanjithAAAA@users.noreply.github.com>
2019-12-10 09:43:48 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-mysql_connect('localhost', 'root', '');
-mysql_select_db('login');
+mysql_connect('mtransport-mysqldbserver.mysql.database.azure.com', 'balaji@mtransport-mysqldbserver', 'Michelin@1234');
+mysql_select_db('michelindb');
 ?>
