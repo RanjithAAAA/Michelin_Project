68123cc99d7f529d5046f85309706be95195162c
Ranjitha <32982928+RanjithAAAA@users.noreply.github.com>
2020-01-14 10:53:42 +0530

    Update addemployee.php

--- addemployee.php
+++ addemployee.php
@@ -1,4 +1,6 @@
 <?php
+ session_start();
+
  $conn = new mysqli("mtransport-mysqldbserver.mysql.database.azure.com","balaji@mtransport-mysqldbserver","Michelin@1234","michelindb");
  
 if(isset($_POST['submit'])) { 
@@ -36,7 +38,7 @@ if(isset($_POST['submit'])) {
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin - Tables</title>
+  <title>Welcome <?= $_SESSION['username'] ?></title>
     <link href="img/icon.jpg" rel="icon">
 
   <!--Registration Form CSS -->
@@ -57,7 +59,7 @@ if(isset($_POST['submit'])) {
 
   <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-    <a class="navbar-brand mr-1" href="admin.php">Welcome Admin</a>
+    <a class="navbar-brand mr-1" href="admin.php">Welcome <?= $_SESSION['username'] ?></a>
 
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars"></i>
